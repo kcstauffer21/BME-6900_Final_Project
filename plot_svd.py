52f9dfcb00558c9bbd2a432d184472ab140bb742
kcstauffer21 <43254582+kcstauffer21@users.noreply.github.com>
2019-11-09 15:22:45 -0700

    verson 2

--- plot_svd.py
+++ plot_svd.py
@@ -108,12 +108,13 @@ def plot_run_svd(data, column_of_interest, v_row):
     column_of_interest_counts = ["n= " + i for i in column_of_interest_counts]
 
     # running stats Mann Whitney
-    if len(data.loc[:, column_of_interest].unique()) > 1:
+    if len(data.loc[:, column_of_interest].unique()) == 2:
         temp_stats = scipy.stats.mannwhitneyu(
             x=v_df[v_df.loc[:, column_of_interest] == v_df.loc[:, column_of_interest].unique()[0]].iloc[:, 0],
             y=v_df[v_df.loc[:, column_of_interest] == v_df.loc[:, column_of_interest].unique()[0]].iloc[:, 0])
 
-        # Making boxplot
+    # Making boxplot
+    if temp_stats[1] < 0.05:
         temp_bp = sns.boxplot(x=v_df.loc[:, column_of_interest], y=v_df.iloc[:, 0])
         pos = range(len(column_of_interest_counts))
         for tick, label in zip(pos, temp_bp.get_xticklabels()):
@@ -128,3 +129,83 @@ def plot_run_svd(data, column_of_interest, v_row):
         print(temp_stats)
 
 
+def plot_run_svd_all(data, columns_of_interest):
+    for i in range(len(columns_of_interest)):
+        for j in range(len(data)):
+            column_of_interest = columns_of_interest[i]
+            # Sorting the data by column of interest
+            data.sort_values(by=column_of_interest, ascending=False, inplace=True)
+            data.reset_index(inplace=True, drop=True)
+            df_svd = data.iloc[:, 1: 12043].transpose().values
+
+            # running svd
+            u, s, v = np.linalg.svd(df_svd, full_matrices=False)
+            s = np.diag(s)
+            print(np.max(np.abs(df_svd - np.linalg.multi_dot([u, s, v]))))
+            print(f"Dimensions of U: {u.shape}, Dimensions of S:{s.shape}, Dimensions of V:{v.shape}")
+            unique, counts = np.unique(np.diag(s), return_counts=True)
+            counted_vals = dict(zip(unique, counts))
+            print(len(counted_vals))
+
+            # Plotting all v,u,s
+            fig, ax = plt.subplots(1, 3)
+            ax0 = sns.heatmap(v[:, :], cmap=cmap, vmin=-0.15, vmax=0.15, ax=ax[0])
+            ax1 = sns.heatmap(u[:, :], cmap=cmap, vmin=-0.10, vmax=0.10, ax=ax[1])
+            ax2 = sns.heatmap(s[:, :], cmap=cmap, center=0, ax=ax[2])
+            ax0.title.set_text('V Transposed Matrix')
+            ax1.title.set_text('U Matrix')
+            ax2.title.set_text('S Matrix')
+            plt.show()
+            # saving figure
+            # plt.savefig(f"./Graphs/Heatmap_All_{column_of_interest}.png")
+
+            # plotting v
+            temp_v_heatmap = sns.heatmap(v[:, :], cmap=cmap, vmin=-0.15, vmax=0.15)
+            plt.title("V Transposed Matrix")
+            plt.show()
+            # Saving figure
+            temp_v_heatmap.figure.savefig(f"./Graphs/Heatmap_V_{column_of_interest}.png")
+
+            # plotting first 5 rows
+            temp_v_heatmap_first_five = sns.heatmap(v[0:5, :], cmap=cmap, center=0)
+            plt.title("V Transposed Matrix First 5 rows")
+            plt.show()
+            # Saving figure
+            temp_v_heatmap_first_five.figure.savefig(f"./Graphs/Heatmap_V_1to5_{column_of_interest}.png")
+
+            # pulling row from v
+            v_df = pd.DataFrame(v[j, :])
+            v_df[column_of_interest] = df_final_wide.loc[:, column_of_interest]
+
+            # Calculation the counts and medians for column_of_interest
+            medians = v_df.groupby(column_of_interest).median().values
+            column_of_interest_counts = v_df[column_of_interest].value_counts().values
+            column_of_interest_counts = [str(x) for x in column_of_interest_counts.tolist()]
+            column_of_interest_counts = ["n= " + i for i in column_of_interest_counts]
+
+            # running stats Mann Whitney
+            if len(data.loc[:, column_of_interest].unique()) > 1:
+                temp_stats = scipy.stats.mannwhitneyu(
+                    x=v_df[v_df.loc[:, column_of_interest] == v_df.loc[:, column_of_interest].unique()[0]].iloc[:, 0],
+                    y=v_df[v_df.loc[:, column_of_interest] == v_df.loc[:, column_of_interest].unique()[0]].iloc[:, 0])
+
+                if temp_stats[1] < 0.05:
+
+                    # Making boxplot
+                    temp_bp = sns.boxplot(x=v_df.loc[:, column_of_interest], y=v_df.iloc[:, 0])
+                    pos = range(len(column_of_interest_counts))
+                    for tick, label in zip(pos, temp_bp.get_xticklabels()):
+                        temp_bp.text(pos[tick], medians[tick] + 0.01, column_of_interest_counts[tick],
+                                     ha='center', fontsize=12, color='w', weight='semibold')
+                    temp_bp.set(ylabel=f"V of {column_of_interest}, Row: {j}", title=f"Boxplot of {column_of_interest}")
+                    temp_bp.text(x=-0.3, y=-0.3, s=f"p={temp_stats[1]}", fontsize=10, ha='center', va='bottom')
+                    plt.show()
+
+                    # Saving figure
+                    temp_bp.figure.savefig(f"./Graphs/bp_{column_of_interest}_Row_{j}.png")
+                    print(temp_stats)
+
+
+# plot_run_svd(df_final_wide, 'gender', 1)
+
+plot_run_svd_all(df_final_wide, df_final_wide.columns[12046:])
